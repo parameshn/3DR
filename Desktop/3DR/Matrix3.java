5ed5e16bb5fcea47895d8fa11207d5f6a857b1d1
paramesh <parameshdarshan1234@gmail.com>
2024-11-22 23:02:13 +0530

    Matrix

---
+++ Desktop/3DR/Matrix3.java
@@ -0,0 +1,31 @@
+public class Matrix3 {
+    double[] values;
+
+    Matrix3(double[] values) {
+        this.values = values;
+    }
+
+    Matrix3 multiply(Matrix3 other) {
+        double[] result = new double[9];
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                for (int i = 0; i < 3; i++) {
+                    result[row * 3 + col] += this.values[row * 3 + i] * other.values[i * 3 + col];
+                    // converts 2D position to 1D array position
+                    // row-major order
+                    // 1D_index = row * width + column
+                }
+            }
+
+        }
+        return new Matrix3(result);
+    }
+
+    Vertex transform(Vertex in)
+    {
+        return new Vertex(in.x * values[0] + in.y * values[3] + in.z * values[6],
+                in.x * values[1] + in.y * values[4] + in.z * values[7],
+                in.x * values[2] + in.y * values[5] + in.z * values[8]);
+    }
+}
+ 
