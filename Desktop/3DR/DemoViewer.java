4c0e311c899855af346f7c89525252b8c135ba61
paramesh <parameshdarshan1234@gmail.com>
2024-11-22 22:58:44 +0530

    drawing all the triangles

--- Desktop/3DR/DemoViewer.java
+++ Desktop/3DR/DemoViewer.java
@@ -13,53 +13,63 @@ public class DemoViewer {
         pane.setLayout(new BorderLayout());
 
         //slider to cotrol horizontal rotation
-        JSlider headingSlider = new JSlider(0,360,180);
+        JSlider headingSlider = new JSlider(0, 360, 180);
         pane.add(headingSlider, BorderLayout.SOUTH);
-        
+
         //slider to control vertical rotation
         JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
         pane.add(pitchSlider, BorderLayout.EAST);
 
+        // List<Triangle> tris = new ArrayList<>();
+        // tris.add(new Triangle(new Vertex(0, 100, 100),
+        //         new Vertex(-100, -100, 100),
+        //         new Vertex(-100, 100, -100),
+        //         Color.WHITE));
+        // tris.add(new Triangle(new Vertex(0, 100, 100),
+        //         new Vertex(-100, -100, 100),
+        //         new Vertex(100, -100, -100),
+        //         Color.RED));
+        // tris.add(new Triangle(new Vertex(-100, 100, -100),
+        //         new Vertex(100, -100, -100),
+        //         new Vertex(0, 100, 100),
+        //         Color.GREEN));
+        // tris.add(new Triangle(new Vertex(-100, 100, -100),
+        //         new Vertex(100, -100, -100),
+        //         new Vertex(-100, -100, 100),
+        //         Color.BLUE));
         List<Triangle> tris = new ArrayList<>();
-        tris.add(new Triangle(new Vertex(100, 100, 100),
-                new Vertex(-100, -100, 100),
+        tris.add(new Triangle(new Vertex(0, 0, 100),
+                new Vertex(-100, -100, 0),
                 new Vertex(-100, 100, -100),
                 Color.WHITE));
-        tris.add(new Triangle(new Vertex(100, 100, 100),
-                new Vertex(-100, -100, 100),
+        tris.add(new Triangle(new Vertex(0, 0, 100),
+                new Vertex(-100, -100, 0),
                 new Vertex(100, -100, -100),
                 Color.RED));
         tris.add(new Triangle(new Vertex(-100, 100, -100),
                 new Vertex(100, -100, -100),
-                new Vertex(100, 100, 100),
+                new Vertex(0, 0, 100),
                 Color.GREEN));
         tris.add(new Triangle(new Vertex(-100, 100, -100),
                 new Vertex(100, -100, -100),
-                new Vertex(-100, -100, 100),
+                new Vertex(-100, -100, 0),
                 Color.BLUE));
 
         //pane to display render results
 
         JPanel renderPanel = new JPanel() {
-           
-            public void paintComponent(Graphics g)
-            {
+
+            public void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
-                
-               
-                
-                
+
                 // Matrix3 transfrom = new Matrix3(new double[] {
                 //         Math.cos(heading), 0, 0 - Math.sin(heading),
                 //         0, 1, 0, Math.sin(heading), 0, Math.cos(heading) });
-                        
 
-              
                 //g2.setColor(Color.WHITE);
-                
 
                 double heading = Math.toRadians(headingSlider.getValue());
 
@@ -79,9 +89,14 @@ public class DemoViewer {
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 
-
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+                //initialize array with extremely far away depths
+                for (int q = 0; q < zBuffer.length; q++) {
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
 
                 for (Triangle t : tris) {
+
                     // Apply transformation to each vertex
                     Vertex v1 = transform.transform(t.v1);
                     Vertex v2 = transform.transform(t.v2);
@@ -89,11 +104,17 @@ public class DemoViewer {
 
                     //since we are not using Graphics2D anymore,
                     // we have to do translation manually
-                   
+
+                    // v1.x += getWidth() / 2;
+                    // v1.y += getHeight() / 2;
+                    // v2.x += getWidth() / 2;
+                    // v2.y += getWidth() / 2;
+                    // v3.x += getWidth() / 2;
+                    // v3.y += getHeight() / 2;
                     v1.x += getWidth() / 2;
                     v1.y += getHeight() / 2;
                     v2.x += getWidth() / 2;
-                    v2.y += getWidth() / 2;
+                    v2.y += getHeight() / 2; // Corrected here
                     v3.x += getWidth() / 2;
                     v3.y += getHeight() / 2;
 
@@ -113,44 +134,46 @@ public class DemoViewer {
                             double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
 
                             if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
-                                img.setRGB(x, y, t.color.getRGB());
+                                // img.setRGB(x, y, t.color.getRGB());
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                int zIndex = y * img.getWidth() + x;
+                                if (zBuffer[zIndex] < depth) {
+                                    img.setRGB(x, y, t.color.getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
+                                // handle rasterization...
+                                // for each rasterized pixel:
+
                             }
                         }
                     }
-                
-
-                    
-                    g2.setColor(t.color);
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y);
-                    path.lineTo(v2.x, v2.y);
-                    path.lineTo(v3.x, v3.y);
                     g2.drawImage(img, 0, 0, null);
-                    path.closePath();
-                    g2.draw(path);
-                    
-
                 }
-
-
-                //rendering magic willl happen here
-
             }
         };
 
-       
-        
-
         pane.add(renderPanel, BorderLayout.CENTER);
-
-        // Add listeners to sliders
         headingSlider.addChangeListener(e -> renderPanel.repaint());
         pitchSlider.addChangeListener(e -> renderPanel.repaint());
-        
+
         frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
-        
-        
     }
+    
+    public static Color getShade(Color color, double shade) {
+        double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+        double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+        double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+        int red = (int) Math.pow(redLinear, 1 / 2.4);
+        int green = (int) Math.pow(greenLinear, 1 / 2.4);
+        int blue = (int) Math.pow(blueLinear, 1 / 2.4);
+
+        return new Color(red, green, blue);
+    }
+
 }
+                    
+                  
